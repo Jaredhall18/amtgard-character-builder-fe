4287aa2ff46b6b2d5054a4a89773d307a325b85f
jaredhall18 <jaredhall18@gmail.com>
2022-02-23 16:09:25 -0700

    CRA, brought in browser router for routes. Deleted excess files.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './Login';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Amtgard!</h1>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
